67c67ff6ed0de92040a118208a9991a771d59555
Joshua Kim <joshuajk7@gmail.com>
2024-02-01 17:08:29 -0800

    Add 01-tree skeleton code.

---
+++ skeleton-code/libtree.h
@@ -0,0 +1,23 @@
+/* This header file provides the public declarations so that
+ * others can use your library.
+ */
+#include <stdbool.h>
+
+/* By convention, exposed library interfaces are prefixed
+ * with the name of the library, in this case "tree_"
+ */
+
+struct
+tree_options {
+  bool all,
+       dirsonly,
+       perms,
+       user,
+       group,
+       skip_hidden,
+       size;
+  enum {NONE, ALPHA, RALPHA, TIME} sort;
+  unsigned int indent;
+};
+
+extern int tree_print(char const *path, struct tree_options opts);
